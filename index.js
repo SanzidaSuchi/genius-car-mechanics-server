cb236d41434633b2d7329f0c4635f0cf0411f13d
Sanzida Suchi <sanzidasuchi356@gmail.com>
2021-10-27 12:31:18 +0600

    add port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config()
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
@@ -66,3 +66,4 @@ app.get('/',(req,res) =>{
 app.listen(port,()=> {
     console.log('Running Genius Server on port', port);
 })
+
